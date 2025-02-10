dbb28c1475217656e5fcd72cebc4b7940cd8cefa
alejandroadk <alejandroadk59@gmail.com>
2025-02-10 11:54:16 -0400

    permission

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,14 +2,25 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from accounts.models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ["username", "email", "name"]
+    list_display = ("name","username", "email",'is_staff')
 
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),  # Campo personalizado
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name','username', 'email', 'password1', 'password2'),
+        }),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/forms.py
+++ accounts/forms.py
@@ -4,13 +4,13 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
-        model=CustomUser
-        fields=UserCreationForm.Meta.fields+ ("name",)
+        model = CustomUser
+        fields = ('name','username', 'email')  
 
         
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields= UserChangeForm.Meta.fields
+        fields = ('name','username', 'email')  
 
